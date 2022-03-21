39a349abd6004217a234ebc2d2ba7d3d1730ac32
Devaun Nicholson <devaunnicholson@Devauns-MacBook-Air-2.local>
2022-03-20 20:03:10 -0700

    made changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,20 +10,40 @@ function Contact() {
             </div>
             <div className='info'>
                 <h3 className='mini-header'> Devaun Nicholson (Lead Programmer) </h3>
+                <div className='container'>
                 <img className='dev-image' src='https://ca.slack-edge.com/T17R4TFAM-U022RTW1403-0a11ec6215b9-512' alt='Devaun Nicholson'/>
-                <a href='https://github.com/devaunthebomb'>
+                <div className='overlay'>
+                    <div className='text font-link'>“Everything negative - pressure, challenges - is
+                    <br/>
+                    all an opportunity for me to rise.”
+                    <br/>
+                    <strong> -Kobe Bryant </strong>
+                    </div>
+                </div>
+                </div>
+                {/* <a href='https://github.com/devaunthebomb'>
                     <img className='github-image' src='https://logos-world.net/wp-content/uploads/2020/11/GitHub-Logo.png' alt='GitHub Logo'/>
                 </a>
                 <a href='https://www.linkedin.com/in/devaun-nicholson-3a0048233/'>
                     <img className='linkedin-image' src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/768px-LinkedIn_logo_initials.png" alt="linkedinlogo"/>
-                </a>
+                </a> */}
             </div>
                 <div className='info'>
                     <h3 className='mini-header'> Daniel Amador (Lead Designer/Programmer) </h3>
+                    <div className='container'>
                     <img className='dev-image' src='https://ca.slack-edge.com/T17R4TFAM-U022RQA0SHH-563fd8e42cf8-512' alt="Daniel Amador"/>
-                    <a href='https://github.com/Artixisthebest'>
+                    <div className='overlay'>
+                        <div className='text font-link'> “The best time to plant a tree was 20 years ago. 
+                        <br/>
+                        The second best time is now.” 
+                        <br/>
+                        <strong> -Chinese Proverb </strong>
+                        </div>
+                    </div>
+                    </div>
+                    {/* <a href='https://github.com/Artixisthebest'>
                         <img className='github-image' src='https://logos-world.net/wp-content/uploads/2020/11/GitHub-Logo.png' alt='GitHub Logo'/>
-                    </a>
+                    </a> */}
                 </div>
         </div>
         //     <h3 class="mini-header">Daniel Amador (Lead Designer/Programmer)</h3>
--- src/components/Navigation-bar.js
+++ src/components/Navigation-bar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import "./Navigation.css"
 
 function Navigation(){
     return(
         <div className="navigation">
         <nav className="navbar navbar-expand bg-purple" >
           <div className="container "  >
+            <div className='WorldRescue'>
             <NavLink className="navbar-brand" to="/">
               <h1 style={{color: "#48cae4"}}> World Rescue </h1>
             </NavLink>
-            <div className='navigation-buttons'>
+            </div>
+            <div className='navigation-buttons nav-b'>
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                   <NavLink className="nav-link" class="newPos"to="/">
